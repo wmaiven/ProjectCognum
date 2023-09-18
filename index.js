3125e82962966e72a481deeab2da66852dc9c9d3
wmaiven <maivencunha@hotmail.com>
2023-09-18 10:23:38 -0300

    criando rota do controller da Api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const API = require("./Api");
+const APICONTROLLER = require("./Api");
 
 // Middleware para permitir solicitações de diferentes origens (CORS)
 app.use(cors());
@@ -10,6 +10,9 @@ app.use(cors());
 // Middleware para fazer o parse de dados no corpo das requisições em formato JSON
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Rotas
+app.use('/', APICONTROLLER);
 
 
 
